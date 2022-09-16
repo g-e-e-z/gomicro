13c830c9b82eef4798524ff5c6ae99d33e0ef11d
g-e-e-z <gordon112524@gmail.com>
2022-09-16 10:01:12 -0600

    refactor with gorilla mux

--- main.go
+++ main.go
@@ -9,14 +9,27 @@ import (
 	"time"
 
 	"github.com/g-e-e-z/gomicro/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
     l := log.New(os.Stdout, "[product-api] ", log.LstdFlags)
     ph := handlers.NewProducts(l)
 
-    sm := http.NewServeMux()
-    sm.Handle("/", ph)
+    // create the handlers
+    sm := mux.NewRouter()
+
+    // create new serve mux and reegister the handlers
+    getRouter := sm.Methods(http.MethodGet).Subrouter()
+    getRouter.HandleFunc("/", ph.GetProducts)
+
+    putRouter := sm.Methods(http.MethodPut).Subrouter()
+    putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+    putRouter.Use(ph.MiddlewareValidateProduct)
+
+    postRouter := sm.Methods(http.MethodPost).Subrouter()
+    postRouter.HandleFunc("/", ph.AddProduct)
+    postRouter.Use(ph.MiddlewareValidateProduct)
 
     s := &http.Server{
         Addr: ":3000",
